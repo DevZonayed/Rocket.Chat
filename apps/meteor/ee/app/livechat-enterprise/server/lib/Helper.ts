DevZonayed/Rocket.Chat

commit fef32f7ba40a1720d28fcac327539dfd33dfe5f9

refactor: Big enterprise file to ts (#29261)

apps/meteor/ee/app/livechat-enterprise/server/lib/LivechatEnterprise.ts
@@ -11,6 +11,12 @@ import {
 	LivechatUnit,
 } from '@rocket.chat/models';
 import { Message } from '@rocket.chat/core-services';
+import type {
+	IOmnichannelBusinessUnit,
+	IOmnichannelRoom,
+	IOmnichannelServiceLevelAgreements,
+	LivechatDepartmentDTO,
+} from '@rocket.chat/core-typings';
 
 import { hasLicense } from '../../../license/server/license';
 import { updateDepartmentAgents } from '../../../../../app/livechat/server/lib/Helper';
@@ -26,7 +32,7 @@ import { AutoCloseOnHoldScheduler } from './AutoCloseOnHoldScheduler';
 import { getInquirySortMechanismSetting } from '../../../../../app/livechat/server/lib/settings';
 
 export const LivechatEnterprise = {
-	async addMonitor(username) {
+	async addMonitor(username: string) {
 		check(username, String);
 
 		const user = await Users.findOneByUsername(username, { projection: { _id: 1, username: 1 } });
@@ -44,7 +50,7 @@ export const LivechatEnterprise = {
 		return false;
 	},
 
-	async removeMonitor(username) {
+	async removeMonitor(username: string) {
 		check(username, String);
 
 		const user = await Users.findOneByUsername(username, { projection: { _id: 1 } });
@@ -66,7 +72,7 @@ export const LivechatEnterprise = {
 		return true;
 	},
 
-	async removeUnit(_id) {
+	async removeUnit(_id: string) {
 		check(_id, String);
 
 		const unit = await LivechatUnit.findOneById(_id, { projection: { _id: 1 } });
@@ -78,7 +84,12 @@ export const LivechatEnterprise = {
 		return LivechatUnit.removeById(_id);
 	},
 
-	async saveUnit(_id, unitData, unitMonitors, unitDepartments) {
+	async saveUnit(
+		_id: string | null,
+		unitData: Omit<IOmnichannelBusinessUnit, '_id'>,
+		unitMonitors: { monitorId: string; username: string },
+		unitDepartments: { departmentId: string }[],
+	) {
 		check(_id, Match.Maybe(String));
 
 		check(unitData, {
@@ -103,7 +114,7 @@ export const LivechatEnterprise = {
 			}),
 		]);
 
-		let ancestors = [];
+		let ancestors: string[] = [];
 		if (_id) {
 			const unit = await LivechatUnit.findOneById(_id);
 			if (!unit) {
@@ -112,13 +123,13 @@ export const LivechatEnterprise = {
 				});
 			}
 
-			ancestors = unit.ancestors;
+			ancestors = unit.ancestors || [];
 		}
 
 		return LivechatUnit.createOrUpdateUnit(_id, unitData, ancestors, unitMonitors, unitDepartments);
 	},
 
-	async removeTag(_id) {
+	async removeTag(_id: string) {
 		check(_id, String);
 
 		const tag = await LivechatTag.findOneById(_id, { projection: { _id: 1 } });
@@ -130,7 +141,7 @@ export const LivechatEnterprise = {
 		return LivechatTag.removeById(_id);
 	},
 
-	async saveTag(_id, tagData, tagDepartments) {
+	async saveTag(_id: string | undefined, tagData: { name: string; description?: string }, tagDepartments: string[]) {
 		check(_id, Match.Maybe(String));
 
 		check(tagData, {
@@ -143,9 +154,9 @@ export const LivechatEnterprise = {
 		return LivechatTag.createOrUpdateTag(_id, tagData, tagDepartments);
 	},
 
-	async saveSLA(_id, slaData) {
+	async saveSLA(_id: string | null, slaData: Pick<IOmnichannelServiceLevelAgreements, 'name' | 'description' | 'dueTimeInMinutes'>) {
 		const oldSLA = _id && (await OmnichannelServiceLevelAgreements.findOneById(_id, { projection: { dueTimeInMinutes: 1 } }));
-		const exists = await OmnichannelServiceLevelAgreements.findDuplicate(_id, slaData.name, slaData.dueTimeInMinutes);
+		const exists = _id && (await OmnichannelServiceLevelAgreements.findDuplicate(_id, slaData.name, slaData.dueTimeInMinutes));
 		if (exists) {
 			throw new Error('error-duplicated-sla');
 		}
@@ -165,7 +176,7 @@ export const LivechatEnterprise = {
 		return sla;
 	},
 
-	async removeSLA(_id) {
+	async removeSLA(_id: string) {
 		const sla = await OmnichannelServiceLevelAgreements.findOneById(_id, { projection: { _id: 1 } });
 		if (!sla) {
 			throw new Error(`SLA with id ${_id} not found`);
@@ -179,7 +190,7 @@ export const LivechatEnterprise = {
 		await removeSLAFromRooms(_id);
 	},
 
-	async placeRoomOnHold(room, comment, onHoldBy) {
+	async placeRoomOnHold(room: IOmnichannelRoom, comment: string, onHoldBy: { _id: string; username?: string; name?: string }) {
 		logger.debug(`Attempting to place room ${room._id} on hold by user ${onHoldBy?._id}`);
 		const { _id: roomId, onHold } = room;
 		if (!roomId || onHold) {
@@ -196,7 +207,7 @@ export const LivechatEnterprise = {
 		return true;
 	},
 
-	async releaseOnHoldChat(room) {
+	async releaseOnHoldChat(room: IOmnichannelRoom) {
 		const { _id: roomId, onHold } = room;
 		if (!roomId || !onHold) {
 			return;
@@ -211,10 +222,17 @@ export const LivechatEnterprise = {
 	 * @param {Partial<import('@rocket.chat/core-typings').ILivechatDepartment>} departmentData
 	 * @param {{upsert?: { agentId: string; count?: number; order?: number; }[], remove?: { agentId: string; count?: number; order?: number; }}} [departmentAgents] - The department agents
 	 */
-	async saveDepartment(_id, departmentData, departmentAgents) {
+	async saveDepartment(
+		_id: string | null,
+		departmentData: LivechatDepartmentDTO,
+		departmentAgents?: {
+			upsert?: { agentId: string; count?: number; order?: number }[];
+			remove?: { agentId: string; count?: number; order?: number };
+		},
+	) {
 		check(_id, Match.Maybe(String));
 
-		const department = _id && (await LivechatDepartmentRaw.findOneById(_id, { projection: { _id: 1, archived: 1 } }));
+		const department = _id ? await LivechatDepartmentRaw.findOneById(_id, { projection: { _id: 1, archived: 1 } }) : null;
 
 		if (!hasLicense('livechat-enterprise')) {
 			const totalDepartments = await LivechatDepartmentRaw.countTotal();
@@ -231,7 +249,7 @@ export const LivechatEnterprise = {
 			});
 		}
 
-		const defaultValidations = {
+		const defaultValidations: Record<string, Match.Matcher<any> | BooleanConstructor | StringConstructor> = {
 			enabled: Boolean,
 			name: String,
 			description: Match.Optional(String),
@@ -302,7 +320,18 @@ export const LivechatEnterprise = {
 const DEFAULT_RACE_TIMEOUT = 5000;
 let queueDelayTimeout = DEFAULT_RACE_TIMEOUT;
 
-const queueWorker = {
+type QueueWorker = {
+	running: boolean;
+	queues: (string | undefined)[];
+	start(): Promise<void>;
+	stop(): Promise<void>;
+	getActiveQueues(): Promise<(string | undefined)[]>;
+	nextQueue(): Promise<string | undefined>;
+	execute(): Promise<void>;
+	checkQueue(queue: string | undefined): Promise<void>;
+};
+
+const queueWorker: QueueWorker = {
 	running: false,
 	queues: [],
 	async start() {
@@ -326,7 +355,7 @@ const queueWorker = {
 	},
 	async getActiveQueues() {
 		// undefined = public queue(without department)
-		return [undefined].concat(await LivechatInquiry.getDistinctQueuedDepartments());
+		return ([undefined] as (undefined | string)[]).concat(await LivechatInquiry.getDistinctQueuedDepartments({}));
 	},
 	async nextQueue() {
 		if (!this.queues.length) {
@@ -368,7 +397,7 @@ const queueWorker = {
 				err: e,
 			});
 		} finally {
-			this.execute();
+			void this.execute();
 		}
 	},
 };
@@ -376,7 +405,7 @@ const queueWorker = {
 let omnichannelIsEnabled = false;
 function shouldQueueStart() {
 	if (!omnichannelIsEnabled) {
-		queueWorker.stop();
+		void queueWorker.stop();
 		return;
 	}
 
@@ -387,16 +416,16 @@ function shouldQueueStart() {
 		} queue`,
 	);
 
-	routingSupportsAutoAssign ? queueWorker.start() : queueWorker.stop();
+	void (routingSupportsAutoAssign ? queueWorker.start() : queueWorker.stop());
 }
 
 RoutingManager.startQueue = shouldQueueStart;
 
-settings.watch('Livechat_enabled', (enabled) => {
+settings.watch<boolean>('Livechat_enabled', (enabled) => {
 	omnichannelIsEnabled = enabled;
-	omnichannelIsEnabled && RoutingManager.isMethodSet() ? shouldQueueStart() : queueWorker.stop();
+	void (omnichannelIsEnabled && RoutingManager.isMethodSet() ? shouldQueueStart() : queueWorker.stop());
 });
 
-settings.watch('Omnichannel_queue_delay_timeout', (timeout) => {
+settings.watch<number>('Omnichannel_queue_delay_timeout', (timeout) => {
 	queueDelayTimeout = timeout < 1 ? DEFAULT_RACE_TIMEOUT : timeout * 1000;
 });

apps/meteor/app/livechat/imports/server/rest/departments.ts
@@ -1,6 +1,7 @@
 import { isGETLivechatDepartmentProps, isPOSTLivechatDepartmentProps } from '@rocket.chat/rest-typings';
 import { Match, check } from 'meteor/check';
 import { LivechatDepartment, LivechatDepartmentAgents } from '@rocket.chat/models';
+import type { ILivechatDepartment } from '@rocket.chat/core-typings';
 
 import { API } from '../../../../api/server';
 import { hasPermissionAsync } from '../../../../authorization/server/functions/hasPermission';
@@ -59,7 +60,7 @@ API.v1.addRoute(
 			});
 
 			const agents = this.bodyParams.agents ? { upsert: this.bodyParams.agents } : {};
-			const department = await LivechatEnterprise.saveDepartment(null, this.bodyParams.department, agents);
+			const department = await LivechatEnterprise.saveDepartment(null, this.bodyParams.department as ILivechatDepartment, agents);
 
 			if (department) {
 				return API.v1.success({

apps/meteor/app/livechat/server/methods/saveDepartment.ts
@@ -10,7 +10,18 @@ declare module '@rocket.chat/ui-contexts' {
 	interface ServerMethods {
 		'livechat:saveDepartment': (
 			_id: string | null,
-			departmentData: Partial<ILivechatDepartment>,
+			departmentData: {
+				enabled: boolean;
+				name: string;
+				description?: string;
+				showOnRegistration: boolean;
+				email: string;
+				showOnOfflineForm: boolean;
+				requestTagBeforeClosingChat?: boolean;
+				chatClosingTags?: string[];
+				fallbackForwardDepartment?: string;
+				departmentsAllowedToForward?: string[];
+			},
 			departmentAgents?:
 				| {
 						agentId: string;

apps/meteor/ee/app/livechat-enterprise/server/lib/routing/LoadBalancing.ts
@@ -1,6 +1,7 @@
 import { Users } from '@rocket.chat/models';
 
 import { RoutingManager } from '../../../../../../app/livechat/server/lib/RoutingManager';
+import type { IRoutingManagerConfig } from '../../../../../../definition/IRoutingManagerConfig';
 
 /* Load Balancing Queuing method:
  *
@@ -8,8 +9,10 @@ import { RoutingManager } from '../../../../../../app/livechat/server/lib/Routin
  * of open chats is paired with the incoming livechat
  */
 class LoadBalancing {
+	private _config: IRoutingManagerConfig;
+
 	constructor() {
-		this.config = {
+		this._config = {
 			previewRoom: false,
 			showConnecting: false,
 			showQueue: false,
@@ -20,7 +23,11 @@ class LoadBalancing {
 		};
 	}
 
-	async getNextAgent(department, ignoreAgentId) {
+	get config(): IRoutingManagerConfig {
+		return this._config;
+	}
+
+	async getNextAgent(department?: string, ignoreAgentId?: string) {
 		const nextAgent = await Users.getNextLeastBusyAgent(department, ignoreAgentId);
 		if (!nextAgent) {
 			return;

apps/meteor/ee/app/livechat-enterprise/server/lib/runEndpointsAsUser.js
@@ -1,18 +1,0 @@
-import { Meteor } from 'meteor/meteor';
-
-const replaceOriginalActionToRunAsUser = (route, endpointsToRunAsUser) => {
-	endpointsToRunAsUser[route.path].forEach((action) => {
-		if (route.endpoints[action]) {
-			const originalAction = route.endpoints[action].action;
-			route.endpoints[action].action = function _internalRouteActionHandler() {
-				return Meteor.runAsUser(this.userId, () => originalAction.apply(this));
-			};
-		}
-	});
-};
-
-export function runEndpointsAsUser({ originalAPIRoutes, endpointsToRunAsUser }) {
-	originalAPIRoutes
-		.filter((route) => Object.keys(endpointsToRunAsUser).includes(route.path))
-		.forEach((route) => replaceOriginalActionToRunAsUser(route, endpointsToRunAsUser));
-}

apps/meteor/ee/server/models/raw/LivechatDepartment.ts
@@ -1,4 +1,4 @@
-import type { ILivechatDepartment, RocketChatRecordDeleted } from '@rocket.chat/core-typings';
+import type { ILivechatDepartment, RocketChatRecordDeleted, LivechatDepartmentDTO } from '@rocket.chat/core-typings';
 import type { ILivechatDepartmentModel } from '@rocket.chat/model-typings';
 import type { Collection, DeleteResult, Document, Filter, FindCursor, FindOptions, UpdateFilter, UpdateResult, Db } from 'mongodb';
 import { LivechatUnit } from '@rocket.chat/models';
@@ -16,7 +16,6 @@ declare module '@rocket.chat/model-typings' {
 			options: FindOptions<ILivechatDepartment>,
 		): Promise<UpdateResult>;
 		unfilteredRemove(query: Filter<ILivechatDepartment>): Promise<DeleteResult>;
-		createOrUpdateDepartment(id: string, data: ILivechatDepartment): Promise<ILivechatDepartment>;
 		removeParentAndAncestorById(id: string): Promise<UpdateResult | Document>;
 		findEnabledWithAgentsAndBusinessUnit(
 			businessUnit: string,
@@ -54,10 +53,8 @@ export class LivechatDepartmentEE extends LivechatDepartmentRaw implements ILive
 		return this.col.deleteOne(query);
 	}
 
-	createOrUpdateDepartment(id: string, data: ILivechatDepartment): Promise<ILivechatDepartment> {
-		data.type = 'd';
-
-		return super.createOrUpdateDepartment(id, data);
+	createOrUpdateDepartment(_id: string | null, data: LivechatDepartmentDTO): Promise<ILivechatDepartment> {
+		return super.createOrUpdateDepartment(_id, { ...data, type: 'd' });
 	}
 
 	removeParentAndAncestorById(id: string): Promise<UpdateResult | Document> {

apps/meteor/ee/server/models/raw/LivechatTag.ts
@@ -27,8 +27,8 @@ export class LivechatTagRaw extends BaseRaw<ILivechatTag> implements ILivechatTa
 	}
 
 	async createOrUpdateTag(
-		_id: string,
-		{ name, description }: { name: string; description: string },
+		_id: string | undefined,
+		{ name, description }: { name: string; description?: string },
 		departments: string[] = [],
 	): Promise<ILivechatTag> {
 		const record = {

apps/meteor/ee/server/models/raw/LivechatUnit.ts
@@ -1,4 +1,4 @@
-import type { IOmnichannelBusinessUnit, ILivechatUnitMonitor, ILivechatDepartment } from '@rocket.chat/core-typings';
+import type { IOmnichannelBusinessUnit, ILivechatDepartment } from '@rocket.chat/core-typings';
 import type { FindPaginated, ILivechatUnitModel } from '@rocket.chat/model-typings';
 import type { FindOptions, Filter, FindCursor, Db, FilterOperators, UpdateResult, DeleteResult, Document, UpdateFilter } from 'mongodb';
 import { LivechatUnitMonitors, LivechatDepartment, LivechatRooms } from '@rocket.chat/models';
@@ -69,13 +69,13 @@ export class LivechatUnitRaw extends BaseRaw<IOmnichannelBusinessUnit> implement
 	}
 
 	async createOrUpdateUnit(
-		_id: string | undefined,
+		_id: string | null,
 		{ name, visibility }: { name: string; visibility: IOmnichannelBusinessUnit['visibility'] },
 		ancestors: string[],
-		monitors: ILivechatUnitMonitor[],
+		monitors: { monitorId: string; username: string }[],
 		departments: { departmentId: string }[],
 	): Promise<Omit<IOmnichannelBusinessUnit, '_updatedAt'>> {
-		monitors = ([] as ILivechatUnitMonitor[]).concat(monitors || []);
+		monitors = ([] as { monitorId: string; username: string }[]).concat(monitors || []);
 		ancestors = ([] as string[]).concat(ancestors || []);
 
 		const record = {

commit 70fb488a13342f1f5956b65b1e68550ff96834d8

regression: Fix typecheck issues caused by #29261 (#29537)

apps/meteor/ee/app/livechat-enterprise/server/lib/Helper.ts
@@ -9,7 +9,7 @@ import {
 } from '@rocket.chat/models';
 import { api } from '@rocket.chat/core-services';
 import type { Document } from 'mongodb';
-import type { ILivechatInquiryRecord, IOmnichannelRoom, IOmnichannelServiceLevelAgreements } from '@rocket.chat/core-typings';
+import type { IOmnichannelRoom, IOmnichannelServiceLevelAgreements, InquiryWithAgentInfo } from '@rocket.chat/core-typings';
 
 import { memoizeDebounce } from './debounceByParams';
 import { settings } from '../../../../../app/settings/server';
@@ -31,12 +31,6 @@ type QueueInfo = {
 	statistics: Document;
 	numberMostRecentChats: number;
 };
-
-type InquiryWithExtraData = Pick<ILivechatInquiryRecord, '_id' | 'rid' | 'name' | 'ts' | 'status' | 'department'> & {
-	position: number;
-	defaultAgent?: { username: string; agentId: string };
-};
-
 export const getMaxNumberSimultaneousChat = async ({ agentId, departmentId }: { agentId?: string; departmentId?: string }) => {
 	if (departmentId) {
 		const department = await LivechatDepartmentRaw.findOneById(departmentId);
@@ -106,10 +100,14 @@ const normalizeQueueInfo = async ({
 	return { spot, message, estimatedWaitTimeSeconds };
 };
 
-export const dispatchInquiryPosition = async (inquiry: InquiryWithExtraData, queueInfo?: QueueInfo) => {
+export const dispatchInquiryPosition = async (inquiry: Omit<InquiryWithAgentInfo, 'v'>, queueInfo?: QueueInfo) => {
 	const { position, department } = inquiry;
+	// Avoid broadcasting if no position was determined
+	if (position === undefined) {
+		return;
+	}
 	const data = await normalizeQueueInfo({ position, queueInfo, department });
-	const propagateInquiryPosition = (inquiry: InquiryWithExtraData) => {
+	const propagateInquiryPosition = (inquiry: Omit<InquiryWithAgentInfo, 'v'>) => {
 		void api.broadcast('omnichannel.room', inquiry.rid, {
 			type: 'queueData',
 			data,
@@ -146,7 +144,7 @@ const dispatchWaitingQueueStatus = async (department?: string) => {
 // but we don't need to notify _each_ change that takes place, just their final position
 export const debouncedDispatchWaitingQueueStatus = memoizeDebounce(dispatchWaitingQueueStatus, 1200);
 
-export const processWaitingQueue = async (department: string | undefined, inquiry: InquiryWithExtraData) => {
+export const processWaitingQueue = async (department: string | undefined, inquiry: InquiryWithAgentInfo) => {
 	const queue = department || 'Public';
 	helperLogger.debug(`Processing items on queue ${queue}`);
 

apps/meteor/ee/app/livechat-enterprise/server/lib/LivechatEnterprise.ts
@@ -16,6 +16,7 @@ import type {
 	IOmnichannelRoom,
 	IOmnichannelServiceLevelAgreements,
 	LivechatDepartmentDTO,
+	InquiryWithAgentInfo,
 } from '@rocket.chat/core-typings';
 
 import { hasLicense } from '../../../license/server/license';
@@ -386,7 +387,7 @@ const queueWorker: QueueWorker = {
 				return;
 			}
 
-			const result = await processWaitingQueue(queue, nextInquiry);
+			const result = await processWaitingQueue(queue, nextInquiry as InquiryWithAgentInfo);
 
 			if (!result) {
 				await LivechatInquiry.unlock(nextInquiry._id);

packages/core-typings/src/IInquiry.ts
@@ -55,3 +55,8 @@ export interface ILivechatInquiryRecord extends IRocketChatRecord {
 	slaId?: string;
 	estimatedWaitingTimeQueue: IOmnichannelServiceLevelAgreements['dueTimeInMinutes'];
 }
+
+export type InquiryWithAgentInfo = Pick<ILivechatInquiryRecord, '_id' | 'rid' | 'name' | 'ts' | 'status' | 'department' | 'v'> & {
+	position?: number;
+	defaultAgent?: { username: string; agentId: string };
+};